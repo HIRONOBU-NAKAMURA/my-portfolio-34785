9e407793ecc181a1ba1e8575a29c784639eb3896
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-18 09:36:09 +0900

    修正

--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @name = current_user.name
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root  'static_pages#index'
-  resources :users, only: :show
+  # resources :users, only: :show
   resources :static_pages, only: [:index,:show]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
