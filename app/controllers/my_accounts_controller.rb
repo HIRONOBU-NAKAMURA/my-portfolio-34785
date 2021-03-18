834c5ad33a8d9ad1d54b19b518f1eff2f9bf1e7d
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-18 12:29:47 +0900

    修正

--- app/controllers/my_accounts_controller.rb
+++ app/controllers/my_accounts_controller.rb
@@ -1,7 +1,7 @@
 class MyAccountsController < ApplicationController
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 20)
   end
   def show
     @user = User.find(params[:id])
