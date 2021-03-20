ce87f672aaa566ed826aa3d0b66ae477ebdbcb87
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-20 14:05:56 +0900

    Rubyドキュメントの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # get 'all_posts/index'
   root  'static_pages#index'
-  # resources :users, only: :show
+  resources :ruby_documents, only: :index
   resources :static_pages, only: [:index,:show]
   resources :my_accounts, only: [:index,:show,:edit,:update] do
     collection do
@@ -11,7 +10,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:show,:create,:destroy] do
     resources :comments, only: :create
   end
-  resources :all_posts, only: [:index]
+  resources :all_posts, only: :index
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'application#hello'
