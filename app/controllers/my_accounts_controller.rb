2f97c5fa6a689d7060c80c9fd698615190e7d762
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-21 14:30:59 +0900

    S3の導入

--- app/controllers/my_accounts_controller.rb
+++ app/controllers/my_accounts_controller.rb
@@ -37,7 +37,6 @@ class MyAccountsController < ApplicationController
   end
 
   def move_to_root
-    @user = User.find(params[:id])
     redirect_to action: :index unless current_user.id == @user.id
   end
 end
