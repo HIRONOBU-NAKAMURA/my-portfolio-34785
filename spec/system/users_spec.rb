9e54eb0f47a3fc12529092f0a3156e6fc15c144a
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-22 14:40:48 +0900

    Userの結合テストの実装

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      visit root_path
+      expect(page).to have_content('新規登録はこちら')
+      visit new_user_registration_path
+      fill_in '名前',with: @user.name
+      fill_in 'メールアドレス',with: @user.email
+      fill_in 'パスワード',with: @user.password
+      fill_in 'パスワード（確認）',with: @user.password_confirmation
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      expect(current_path).to eq(root_path)
+      expect(page).to have_no_content('新規登録はこちら')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録はこちら')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in '名前',with: ''
+      fill_in 'メールアドレス',with: ''
+      fill_in 'パスワード',with: ''
+      fill_in 'パスワード（確認）',with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq('/users')
+    end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      visit root_path
+      expect(page).to have_content('ログイン')
+      visit new_user_session_path
+      fill_in 'メールアドレス',with: @user.email
+      fill_in 'パスワード',with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+      expect(page).to have_no_content('新規登録はこちら')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインできない' do
+      visit root_path
+      expect(page).to have_content('ログイン')
+      visit new_user_session_path
+      fill_in 'メールアドレス',with: ''
+      fill_in 'パスワード',with: ''
+      find('input[name="commit"]').click
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
