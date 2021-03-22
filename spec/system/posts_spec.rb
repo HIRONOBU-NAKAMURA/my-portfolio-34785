7663226f3dc800bb9397f70a6dbfa7195bf24abf
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-22 17:15:30 +0900

    コメントの結合テストの実装

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -7,11 +7,7 @@ RSpec.describe "コンテンツの投稿", type: :system do
   end
   context '投稿ができるとき' do
     it 'ログインしたユーザーは新規登録ができる' do
-      visit new_user_session_path
-      fill_in 'メールアドレス', with: @user.email
-      fill_in 'パスワード', with: @user.password
-      find('input[name="commit"]').click
-      expect(current_path).to eq(root_path)
+      sign_in(@user)
       expect(page).to have_content('投稿')
       fill_in 'post_content', with: @post.content
       attach_file 'post[image]',"public/images/test_image.png"
@@ -35,11 +31,7 @@ RSpec.describe 'コンテンツのコメントページ', type: :system do
     @post = FactoryBot.create(:post)
   end
   it 'ログインしたユーザーはコンテンツコメントページに遷移してコメント投稿欄が表示される' do
-    visit new_user_session_path
-    fill_in 'メールアドレス', with: @post.user.email
-    fill_in 'パスワード', with: @post.user.password
-    find('input[name="commit"]').click
-    expect(current_path).to eq root_path
+    sign_in(@post.user)
     visit post_path(@post)
     expect(page).to have_content("#{@post.content}")
     expect(page).to have_selector 'form'
